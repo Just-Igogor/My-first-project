241614be2ff0421656b3f84931a9d1d8f869ef7b
Смелый <149298913+Just-Igogor@users.noreply.github.com>
2024-02-10 15:19:54 +0300

    Контрольная Пивота

---
+++ Kontrol/Kontrol/Task_A.c
@@ -0,0 +1,69 @@
+/*ЮФУ, ИКТИБ, МОП ЭВМ
+Программирование и основы теории алгоритмов
+Лаба 6.1
+КТбо1-6, Смелов Игорь Александрович
+Задание A
+15.12.2023*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+	int N, Res[256], * ptr1 = NULL, * ptr2 = NULL, i, j;
+	char c;
+	i = j = 0;
+
+	scanf("%d", &N);
+
+	ptr1 = (char*)calloc(N, sizeof(int));
+	ptr2 = (char*)calloc(N, sizeof(int));
+
+	if (!ptr1 || !ptr2) {
+		exit(1);
+	}
+	for (int k = 0; k <= N; k++) {
+		Res[k] = -1;
+	}
+
+	do {
+		scanf("%d%c", ptr1 + i, &c);
+		i++;
+	} while (c != '\n');
+	do {
+		scanf("%d%c", ptr2 + j, &c);
+		j++;
+	} while (c != '\n');
+
+	for (int k = 0; k < i; k++) {
+		Res[*(ptr1 + k)] = 1;
+	}
+
+	for (int k = 0; k < j; k++) {
+		if (Res[*(ptr2 + k)] == -1) {
+			Res[*(ptr2 + k)] = 0;
+		}
+		else if (Res[*(ptr2 + k)] == 1) {
+			Res[*(ptr2 + k)] = 2;
+		}
+	}
+
+	for (int k = 1; k <= N; k++) {
+		if (Res[k] == 2) {
+			printf("%d ", k);
+		}
+	}
+	printf("\n");
+	for (int k = 1; k <= N; k++) {
+		if (Res[k] == -1) {
+			printf("%d ", k);
+		}
+	}
+	printf("\n");
+
+	free(ptr1);
+	free(ptr2);
+
+	return 0;
+}
